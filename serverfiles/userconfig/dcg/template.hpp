e13b518cdfe102956dac31f6ea65e183a77969c0
Diveyez <nemofbaltimore@gmail.com>
2019-04-01 08:46:46 -0700

    .

---
+++ serverfiles/userconfig/dcg/template.hpp
@@ -0,0 +1,115 @@
+class dcg_main_blacklistLocations {
+	typeName = "ARRAY";
+	value[] = {
+	};
+};
+class dcg_main_unitPoolEast {
+	typeName = "POOL";
+	value[] = {
+		};
+};
+class dcg_main_vehPoolEast {
+	typeName = "POOL";
+	value[] = {
+		};
+};
+class dcg_main_airPoolEast {
+	typeName = "POOL";
+	value[] = {
+		};
+};
+class dcg_main_sniperPoolEast {
+	typeName = "POOL";
+	value[] = {
+		};
+};
+class dcg_main_officerPoolEast {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_artyPoolEast {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_unitPoolInd {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_vehPoolInd {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_airPoolInd {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_sniperPoolInd {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_officerPoolInd {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_artyPoolInd {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_unitPoolWest {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_vehPoolWest {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_airPoolWest {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_sniperPoolWest {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_officerPoolWest {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_artyPoolWest {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_unitPoolCiv {
+	typeName = "POOL";
+	value[] = {
+  };
+};
+class dcg_main_vehPoolCiv {
+	typeName = "POOL";
+	value[] = {
+  };
+};
+class dcg_main_airPoolCiv {
+	typeName = "POOL";
+	value[] = {
+	};
+};
+class dcg_main_vipPoolCiv {
+	typeName = "POOL";
+	value[] = {
+	};
+};
